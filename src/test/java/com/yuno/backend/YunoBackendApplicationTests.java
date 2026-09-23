fca90174cd86b0a4cdae6e1b1de7830b9a179339
Anvith433 <anvithshetty932@gmail.com>
2026-09-23 09:12:28 +0100

    chore: initialize YUNO backend project

---
+++ src/test/java/com/yuno/backend/YunoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuno.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
